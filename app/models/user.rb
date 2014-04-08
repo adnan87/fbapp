b7300d7954b6d84cdd2145bc3bf576d771f7221c
Sunil <sunil.silumala@wwindia.com>
2014-04-08 14:04:46 +0530

    Fixed Issue for FB login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,13 @@ class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:facebook]
 
 
+  
   def self.find_for_facebook_oauth(auth,signed_in_resource=nil)
   where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
-      #user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0,20]
       #user.name = auth.info.name   # assuming the user model has a name
       #user.image = auth.info.image # assuming the user model has an image
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Parentful::Application.routes.draw do
   resources :posts
-
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
